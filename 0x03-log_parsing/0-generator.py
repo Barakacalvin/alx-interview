7dc12a11fcb6eb88ed7c4903b503bc87abca41f5
Calvin Baraka <30375880+Barakacalvin@users.noreply.github.com>
2023-07-19 23:37:03 +0300

    Update 0-generator.py

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
 '''A script that generates random HTTP request logs.
 '''
-import random
 import sys
-import datetime
-from time import sleep
+import signal
 
+status_codes = {200, 301, 400, 401, 403, 404, 405, 500}
+total_size = 0
+lines_by_status = {code: 0 for code in status_codes}
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET {} {}\" {} {}\n".format(
-        random.randint(1, 255),
-        random.randint(1, 255),
-        random.randint(1, 255),
-        random.randint(1, 255),
-        datetime.datetime.now(),
-        '/projects/1216',
-        'HTTP/1.1',
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+def print_statistics(signum=None, frame=None):
+    print("Total file size:", total_size)
+    for code in sorted(lines_by_status.keys()):
+        if lines_by_status[code] > 0:
+            print(f"{code}: {lines_by_status[code]}")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, print_statistics)
+
+try:
+    for line_number, line in enumerate(sys.stdin, start=1):
+        try:
+            _, _, _, _, _, status_code, file_size_str = line.split()[3:10]
+            status_code = int(status_code)
+            file_size = int(file_size_str)
+            if status_code in status_codes:
+                lines_by_status[status_code] += 1
+                total_size += file_size
+
+            if line_number % 10 == 0:
+                print_statistics()
+        except ValueError:
+            # Skip the line if it doesn't match the expected format
+            continue
+except KeyboardInterrupt:
+    print_statistics()
